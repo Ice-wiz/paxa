72fbe6c106c4b250a6f6aeb801f734de225f0d1e
ice-wiz <img_2020012@iiitm.ac.in>
2024-07-13 16:36:30 +0530

    not done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
         <CardList3/>
         <CardList4/>
         <CardList5/>
-        <div className="w-10 lg:w-[2.5rem] h-full bg-transparent invisible">heyyy</div>
+               <div className="w-10 lg:w-[2.5rem] h-full bg-transparent invisible">heyyy</div>
       </div>
     </div>
   );
@@ -33,36 +33,3 @@ const Home = () => {
 
 // Exporting the Home component as the default export
 export default Home;
-
-
-
-
-
-
-// import Header1 from '../components/HeaderComponents/Header1';
-// import Header2 from '../components/HeaderComponents/Header2';
-// import CardList1 from '../components/CardsLists/CardList1';
-// import CardList2 from '../components/CardsLists/CardList2';
-// import CardList3 from '../components/CardsLists/CardList3';
-// import CardList4 from '../components/CardsLists/CardList4';
-// import CardList5 from '../components/CardsLists/CardList5';
-
-// const Home = () => {
-//   return (
-//     <div className="overflow-x-auto overflow-y-auto h-screen">
-//       <Header1 />
-//       <Header2 />
-  
-//       <div className="flex flex-col lg:flex-row justify-center lg:justify-evenly items-center lg:items-start max-w-none lg:max-w-screen-xl mt-2 px-4">
-//         <CardList1 />
-//         <CardList2 />
-//         <CardList3 />
-//         <CardList4 />
-//         <CardList5 />
-//         <div className="w-10 lg:w-[2.5rem] h-full bg-transparent invisible">heyyy</div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Home;
